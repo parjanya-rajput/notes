eecefba1385660c244aa17ebefd18bcece1624e9
Parjanya Rajput <pajuraj05@gmail.com>
2023-06-23 17:29:32 +0530

    AuthService and AuthProvider

---
+++ lib/services/auth/auth_user.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart' show User;
+import 'package:flutter/cupertino.dart';
+
+@immutable //Internals are never changed moreover the child class also cannot have changeable internals
+class AuthUser{
+
+  final bool isEmailVerified;
+  const AuthUser(this.isEmailVerified);
+
+  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified); //Factory constructors are like shared/cached constructor saves memory and time
+}
